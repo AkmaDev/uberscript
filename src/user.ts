25405b824043fe7ba61db45d2e9f32e78fb2b6de
AkmaDev <manews193@gmail.com>
2026-03-13 14:38:33 +0100

    classes User

---
+++ src/user.ts
@@ -0,0 +1,26 @@
+type Meal = {
+  id: number
+  name: string
+  calories: number
+  price: number
+}
+
+type Order = {
+  id: number
+  meals: Meal[]
+  total: number
+}
+
+class User {
+  id: number
+  name: string
+  wallet: number
+  orders: Order[]
+
+  constructor(id: number, name: string, wallet: number) {
+    this.id = id
+    this.name = name
+    this.wallet = wallet
+    this.orders = []
+  }
+}
